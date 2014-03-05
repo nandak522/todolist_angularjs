c42d55fb577047430eff64752adf9acabeffcb5b
Nanda Kishore <jnandakishore@zeomega.com>
2014-03-05 17:54:07 +0530

    Initial Commit. Todo list app

---
+++ scripts/controllers.js
@@ -0,0 +1,24 @@
+angular.module('todolistApp')
+  .controller('listController', ['TodoItemsService', function(TodoItemsService){
+    var self = this;
+
+    self.getTodoItems = TodoItemsService.list;
+
+    self.getItemStatus = function(itemIndex){
+      return TodoItemsService.get(itemIndex).done ? 'completed' : 'not'
+    };
+
+    self.deleteItem = function (itemIndex) {
+      TodoItemsService.remove(itemIndex);
+    };
+
+  }])
+  .controller('addItemController', ['$scope', 'TodoItemsService', function($scope, TodoItemsService){
+    var self = this;
+
+    self.addItem = function () {
+      TodoItemsService.add(self.newTodoItem);
+      self.newTodoItem = '';
+    };
+
+  }]);
---
+++ scripts/services.js
@@ -0,0 +1,25 @@
+angular.module('todolistApp')
+  .factory('TodoItemsService', [function () {
+
+    var todoItems = [
+      {'task': 'Learning scopes', 'done': false},
+      {'task': 'Learning services', 'done': false},
+      {'task': 'Learning providers', 'done': true}
+    ];
+
+    return {
+      add: function(item){
+        todoItems.push({'task': item, 'done': false});
+      },
+      remove: function (index) {
+        todoItems.splice(index, 1);
+      },
+      list: function () {
+        return todoItems;
+      },
+      get: function (index) {
+        return todoItems[index];
+      }
+      // TODO: done functionality
+    }
+  }]);
